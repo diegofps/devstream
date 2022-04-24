6001b42ebc1395277ad59056b405045a4556f231
Diego Souza <diegofpsouza@gmail.com>
2022-04-24 00:47:07 -0300

    Adding initial script

---
+++ main.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+
+from evdev import InputDevice, UInput, AbsInfo, list_devices, ecodes as e
+import time
+
+def grab_device():
+    devices = [InputDevice(path) for path in list_devices()]
+    for d in devices:
+        if d.name == "Logitech USB Trackball":
+            d.grab()
+            return d
+    return None
+
+class Key:
+    def __init__(self, name, device, type, code, scan=None):
+        self.device = device
+        self.scan = scan
+        self.name = name
+        self.type = type
+        self.code = code
+        self.value = 0
+    
+    def update(self, value):
+        if value == self.value:
+            return
+        
+        self.value = value
+
+        #print("Updating", self.name, self.type, self.code, self.value, self.scan)
+
+        if self.scan is not None:
+            self.device.write(e.EV_MSC, e.MSC_SCAN, self.scan)
+        
+        self.device.write(self.type, self.code, self.value)
+        self.device.write(e.EV_SYN, e.SYN_REPORT, 0)
+
+
+class WheelKey:
+    def __init__(self, name, device, type, code, code_high, speed):
+        self.device = device
+        self.speed = speed
+        self.name = name
+        self.type = type
+        self.code = code
+        self.code_high = code_high
+        self.value = 0
+        self.cumulative = 0
+    
+    def update(self, value):
+        if value == self.value:
+            return
+        
+        self.value = value
+
+        #print("Updating", self.name, self.type, self.code, self.value, self.scan)
+
+        value = self.value * self.speed
+
+        self.cumulative += value
+        self.device.write(self.type, self.code_high, value)
+        
+        while self.cumulative >= 120:
+            self.device.write(self.type, self.code, 1)
+            self.cumulative -= 120
+
+        while self.cumulative <= 0:
+            self.device.write(self.type, self.code, -1)
+            self.cumulative += 120
+
+        self.device.write(e.EV_SYN, e.SYN_REPORT, 0)
+    
+cap = {
+    e.EV_KEY : [e.BTN_LEFT, e.BTN_RIGHT, e.BTN_MIDDLE, e.BTN_SIDE, e.BTN_EXTRA],
+    e.EV_REL : [e.REL_X, e.REL_Y, e.REL_WHEEL, e.REL_HWHEEL, e.REL_WHEEL_HI_RES, e.REL_HWHEEL_HI_RES]
+}
+
+vdev = UInput(cap, name="virtualmarble", version=0x3)
+
+bt_right   = Key("right",   vdev, e.EV_KEY, e.BTN_RIGHT, 90001)
+bt_left    = Key("left",    vdev, e.EV_KEY, e.BTN_LEFT, 90004)
+bt_middle  = Key("middle",  vdev, e.EV_KEY, e.BTN_MIDDLE, 90005)
+bt_rel_x   = Key("rel_x",   vdev, e.EV_REL, e.REL_X)
+bt_rel_y   = Key("rel_y",   vdev, e.EV_REL, e.REL_Y)
+bt_back    = Key("back",    vdev, e.EV_KEY, e.BTN_SIDE, 90004)
+bt_forward = Key("forward", vdev, e.EV_KEY, e.BTN_EXTRA, 90005)
+bt_wheel_h = WheelKey("wheel_h", vdev, e.EV_REL, e.REL_HWHEEL, e.REL_HWHEEL_HI_RES, 20)
+bt_wheel_v = WheelKey("wheel_v", vdev, e.EV_REL, e.REL_WHEEL, e.REL_WHEEL_HI_RES, 10)
+
+while True:
+
+    dev = grab_device()
+
+    if dev is None:
+        print("Trackball not found, waiting for it...")
+        time.sleep(3)
+        continue
+    
+    done = False
+    
+    left_big    = 0
+    right_big   = 0
+    left_small  = 0
+    right_small = 0
+    in_rel_x    = 0
+    in_rel_y    = 0
+
+    while not done:
+        event = dev.read_one()
+
+        if event is None:
+            time.sleep(0.001)
+            continue
+        
+        if event.type == e.EV_SYN:
+            if event.code == e.SYN_REPORT:
+                if right_big == 1:
+                    #print("alternate state")
+                    bt_right.update(0)
+                    bt_left.update(0)
+                    bt_middle.update(0)
+                    bt_rel_x.update(0)
+                    bt_rel_y.update(0)
+                    bt_back.update(left_big)
+                    bt_forward.update(left_small)
+
+
+                    bt_wheel_h.update(in_rel_x)
+                    bt_wheel_v.update(-in_rel_y)
+
+                    # if abs(in_rel_x) > abs(in_rel_y):
+                    #     bt_wheel_h.update(in_rel_x)
+                    # else:
+                    #     bt_wheel_v.update(-in_rel_y)
+
+                else:
+                    #print("normal state")
+                    bt_left.update(left_big)
+                    bt_right.update(left_small)
+                    bt_middle.update(right_small)
+                    bt_rel_x.update(in_rel_x)
+                    bt_rel_y.update(in_rel_y)
+                    bt_wheel_h.update(0)
+                    bt_wheel_v.update(0)
+                    bt_back.update(0)
+                    bt_forward.update(0)
+                
+                in_rel_x = 0
+                in_rel_y = 0
+
+        elif event.type == e.EV_KEY:
+            if event.code == e.BTN_LEFT:
+                left_big = event.value
+                
+            elif event.code == e.BTN_RIGHT:
+                right_big = event.value
+
+            elif event.code == e.BTN_EXTRA:
+                right_small = event.value
+
+            elif event.code == e.BTN_SIDE:
+                left_small = event.value
+                
+
+        elif event.type == e.EV_REL:
+            if event.code == e.REL_X:
+                in_rel_x = event.value
+
+            elif event.code == e.REL_Y:
+                in_rel_y = event.value
+
